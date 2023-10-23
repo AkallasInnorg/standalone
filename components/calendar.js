704e4b894166757d22a1cb272697ab4f7d2f8ccb
AkallasInnorg <akallas@innorg.it>
2023-10-23 15:12:43 +0200

    calendar improvement

--- components/calendar.js
+++ components/calendar.js
@@ -1,16 +1,22 @@
 import React, {useState} from 'react';
 import {Calendar, LocaleConfig} from 'react-native-calendars';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function MyCalendar () {
   const [selected, setSelected] = useState('');
 
   return (
     <Calendar
+      renderArrow={(left)=><Ionicons name='chevron-back-outline'/>}
+      
+      enableSwipeMonths={true}
       onDayPress={day => {
         setSelected(day.dateString);
+        console.log(day.dateString)
       }}
       markedDates={{
         [selected]: {
+            marked: true,
             selected: true, 
             disableTouchEvent: true, 
             selectedDotColor: 'orange',}
